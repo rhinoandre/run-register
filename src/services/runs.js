5718f40507c978faca90c035ca417604ffed2d59
abazoli <andre.bazoli@watchguard.com>
2018-06-20 21:56:48 -0300

    Feat: Adding all runs' API

--- src/services/runs.js
+++ src/services/runs.js
@@ -1,8 +1,46 @@
+import moment from 'moment';
+
 export default function(BASE_URL) {
   return {
     getAll(token) {
       return fetch(`${BASE_URL}/runs`, {
         method: 'GET',
+        headers: {
+          'Content-Type': 'application/type',
+          Authorization: `Bearer ${token}`
+        }
+      });
+    },
+    getById(id, token) {
+      return fetch(`${BASE_URL}/runs/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/type',
+          Authorization: `Bearer ${token}`
+        }
+      });
+    },
+    delete(id, token) {
+      return fetch(`${BASE_URL}/runs/${id}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    },
+    update({ id, friendly_name, duration, distance }, token) {
+      return fetch(`${BASE_URL}/runs/${id}`, { id, friendly_name, duration, distance }, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/type',
+          Authorization: `Bearer ${token}`
+        }
+      });
+    },
+    create({ friendly_name, duration, distance }, token) {
+      const created = moment().format('YYYY-MM-DD HH:mm:SS');
+      return fetch(`${BASE_URL}/runs`, { friendly_name, duration, distance, created }, {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/type',
           Authorization: `Bearer ${token}`
