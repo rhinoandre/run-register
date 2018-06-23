d95a54286d7d050e0c13cf4ddb510e83c6be5b75
abazoli <andre.bazoli@watchguard.com>
2018-06-23 19:35:26 -0300

    Adding user_id to run update object

--- src/services/runs.js
+++ src/services/runs.js
@@ -28,8 +28,8 @@ export default function(BASE_URL) {
         }
       });
     },
-    update({ id, friendly_name, duration, distance }, token) {
-      return fetch(`${BASE_URL}/runs/${id}`, { id, friendly_name, duration, distance }, {
+    update({ id, user_id, friendly_name, duration, distance }, token) {
+      return fetch(`${BASE_URL}/runs/${id}`, { id, user_id, friendly_name, duration, distance }, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/type',
